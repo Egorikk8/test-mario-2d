7e6ac22431330e9de015e6902a5f62b4f53db146
Egorikk8 <92586155+Egorikk8@users.noreply.github.com>
2021-10-26 17:29:16 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ let picPlayerLeft = new Image();
 picPlayerLeft.src = "./pic/playerLeft.png";
 let picPlayerRight = new Image();
 picPlayerRight.src = "./pic/playerRight.png";
+
 let arrPicPlayer = [];
 arrPicPlayer['left'] = picPlayerLeft;
 arrPicPlayer['right'] = picPlayerRight;
 
-let picEnemy = new Image();
-picEnemy.src = "./pic/enemy.png";
+let picEnemyLeft = new Image();
+picEnemyLeft.src = "./pic/enemyLeft.png";
+let picEnemyRight = new Image();
+picEnemyRight.src = "./pic/enemyRight.png";
+
+let arrPicEnemy = [];
+arrPicEnemy['left'] = picEnemyLeft;
+arrPicEnemy['right'] = picEnemyRight;
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
@@ -32,25 +39,62 @@ function resizeImg(img, percent) {
         img.height = img.width * prop;
     }
 }
-let xPlayer = 50,
-    yPlayer = 810,
-    speedPlayer = 5,
-    navPlayer = 'right'
-xEnemy = 750,
-    yEnemy = 845;
+let startGame = false;
+let xPlayer = 50, yPlayer = 740,
+    speedPlayer = 5, speedEnemy = 25,
+    navPlayer = 'right', navEnemy='right',
+    xEnemy = 750, yEnemy = 765;
+
+    
+    function boardPic(pic, x , y){
+        let picRight, picBottom;
+        picRight = x + pic.width;
+        picBottom = y + pic.height;
+        return [picRight , picBottom]
+    }
 
 function draw() {
-    let picPlayer = arrPicPlayer[navPlayer]
+    let picPlayer = arrPicPlayer[navPlayer];
+    let picEnemy = arrPicEnemy[navEnemy];
     resizeImg(picPlayer, 8);
     resizeImg(picEnemy, 5);
+    let boardPicPlayer = boardPic(picPlayer,xPlayer,yPlayer);
+    let boardPicEnemy = boardPic(picEnemy,xEnemy,yEnemy);
+
+    function startPosition(){
+        xPlayer = window.innerWidth*0.05;
+        yPlayer = window.innerHeight*0.93-picPlayer.height;
+
+        xEnemy = window.innerWidth*0.85;
+        yEnemy = window.innerHeight*0.93-picEnemy.height;
+    }
+
+    function moveEnemy(){
+        let timerMoveEnemy = setTimeout(()=>{
+            if(xEnemy>(xPlayer+picPlayer.width)){
+                xEnemy--;
+                navEnemy='left'
+            }else if((xEnemy+picEnemy.width)<xPlayer){
+                xEnemy++;
+                navEnemy='right'
+            }
+            draw();
+         moveEnemy();
+         },speedEnemy);
+        }
+        if(startGame){
+            startPosition();
+            moveEnemy();
+            startGame=false;
+        }
     ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
     ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width, picPlayer.height);
     ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width, picEnemy.height);
 }
 
-picEnemy.onload = picPlayerLeft.onload = picPlayerRight.onload = picBackground.onload = draw;
+picEnemyRight.onload = picPlayerLeft.onload = picPlayerLeft.onload = picBackground.onload = draw;
 
-document.addEventListener('keyup', (event)=>{
+document.addEventListener('keydown', (event)=>{
     let KeyPressed = event.code;
     switch (KeyPressed) {
         case 'ArrowLeft':
@@ -63,10 +107,13 @@ document.addEventListener('keyup', (event)=>{
             navPlayer = 'right';
             soundStep.play();
             break;
+            case 'Enter':
+                startGame=true;
+                break
     }
     draw();
 });
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keyup', (event) => {
     let KeyPressed = event.code;
     switch (KeyPressed) {
         case 'ArrowLeft':
