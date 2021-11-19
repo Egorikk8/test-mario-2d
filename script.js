6238373d34edc91dc499e514fcdc224265033272
Egorikk8 <92586155+Egorikk8@users.noreply.github.com>
2021-11-19 17:18:23 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let ctx = canvas.getContext('2d');
 
 let soundStep = new Audio('./sound/step.mp3');
 
+//--------------------Картинки------------------//
 let picBackground = new Image();
 picBackground.src = "./pic/background.jpg";
 
@@ -15,6 +16,7 @@ let arrPicPlayer = [];
 arrPicPlayer['left'] = picPlayerLeft;
 arrPicPlayer['right'] = picPlayerRight;
 
+//-----------Контейнеры--------------//
 let picEnemyLeft = new Image();
 picEnemyLeft.src = "./pic/enemyLeft.png";
 let picEnemyRight = new Image();
@@ -34,14 +36,15 @@ arrPicEnemy['right'] = picEnemyRight;
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-function generateRAndomPosition(pic, xMin, xMax, yMin, yMax){
-    let x = Math.random()*((xMax-pic.width)-xMin)+xMin,
-    y=Math.random()*((yMax-pic.height)-yMin)+yMin;
-    return[x,y];
+function generateRAndomPosition(pic, xMin, xMax, yMin, yMax) {
+    let x = Math.random() * ((xMax - pic.width) - xMin) + xMin,
+        y = Math.random() * ((yMax - pic.height) - yMin) + yMin;
+    return [x, y];
 }
-let positionCoin=[0,0];
-function newPositionCoin(){
-    positionCoin=generateRAndomPosition(picCoin,0,innerWidth,innerHeight*0.7,innerHeight*0.88);
+let positionCoin = [0, 0];
+
+function newPositionCoin() {
+    positionCoin = generateRAndomPosition(picCoin, 0, innerWidth, innerHeight * 0.7, innerHeight * 0.88);
 }
 
 function resizeImg(img, percent) {
@@ -56,114 +59,154 @@ function resizeImg(img, percent) {
         img.height = img.width * prop;
     }
 }
+
+//-----------------Переменные------------//
 let startGame = false;
-let xPlayer = 50, yPlayer = 740,
-    speedPlayer = 5, speedEnemy = 25,
-    navPlayer = 'right', navEnemy='right',
-    xEnemy = 750, yEnemy = 765, boardPicPlayer, boardPicEnemy, boardPicCoin, 
-    countLife = 3; countCoin=0;
+let xPlayer = 50,
+    yPlayer = 740,
+    speedPlayer = 5,
+    speedEnemy = 25,
+    navPlayer = 'right',
+    navEnemy = 'right',
+    xEnemy = 750,
+    yEnemy = 765,
+    boardPicPlayer, boardPicEnemy, boardPicCoin,
+    countLife = 3;
+countCoin = 0;
 
-    
-    function boardPic(pic, x , y){
-        let picRight, picBottom;
-        picRight = x + pic.width;
-        picBottom = y + pic.height;
-        return [picRight , picBottom]
-    }
 
-    function checkCollision(x1,x2,y1,y2,r1,r2,b1,b2){
-        if(r1>x2 && r2>x1 && b1>y2 && b2>y1){
-            return true;
-        }
-        else{
-            return false;
-        }
+function boardPic(pic, x, y) {
+    let picRight, picBottom;
+    picRight = x + pic.width;
+    picBottom = y + pic.height;
+    return [picRight, picBottom]
+}
+
+function checkCollision(x1, x2, y1, y2, r1, r2, b1, b2) {
+    if (r1 > x2 && r2 > x1 && b1 > y2 && b2 > y1) {
+        return true;
+    } else {
+        return false;
     }
+}
 
 function draw() {
-    let picPlayer = arrPicPlayer[navPlayer];
-    let picEnemy = arrPicEnemy[navEnemy];
-    resizeImg(picPlayer, 8);
-    resizeImg(picEnemy, 5);
-    resizeImg(picLife, 5)
-    resizeImg(picCoin, 5)
-     boardPicPlayer = boardPic(picPlayer,xPlayer,yPlayer);
-     boardPicEnemy = boardPic(picEnemy,xEnemy,yEnemy);
-     boardPicCoin = boardPic(picCoin,positionCoin[0],positionCoin[1]);
+    let buttonStart = new Path2D;
 
+    function drawButton(strokeColor, fillColor) {
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+        ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
+        let wightButton = innerWidth * 0.2,
+            heightButton = innerHeight * 0.1;
+        let xButton = innerWidth * 0.5 - wightButton / 2,
+            yButton = innerHeight * 0.5 - heightButton / 2;
+        buttonStart.rect(xButton, yButton, wightButton, heightButton);
 
-    function startPosition(){
-        xPlayer = window.innerWidth*0.05;
-        yPlayer = window.innerHeight*0.93-picPlayer.height;
-
-        xEnemy = window.innerWidth*0.85;
-        yEnemy = window.innerHeight*0.93-picEnemy.height;
+        function fillButton() {
+            ctx.strokeStyle = strokeColor;
+            ctx.fillStyle = fillColor;
+            ctx.lineWight = 5;
+            ctx.stroke(buttonStart)
+            ctx.fill(buttonStart)
+        }
+        fillButton();
+        printText('Start', xButton + wightButton / 2 - 45, yButton + heightButton / 2 + 10, 40, 'Black')
     }
 
-    function moveEnemy(){
-        function collisionEnemy(){
-            if(boardPicPlayer[0]>xEnemy && xPlayer<boardPicEnemy[0] && yPlayer<boardPicEnemy[1] && yEnemy<boardPicPlayer[1]){
-                countLife--;
-                if(countLife<=0){
-                    let newGame = confirm('Gameover:(\n Хотите начать заново?');
-                    if(newGame){
-                        countLife=3;
-                    }else{
-                        clearTimeout(timerMoveEnemy);
-                        startPosition();
-                        return;
+    function printText(text, x, y, size, color) {
+        ctx.font = size + "px Arial";
+        ctx.fillStyle = color;
+        ctx.fillText(text, x, y);
+    }
+    if (startGame) {
+        let picPlayer = arrPicPlayer[navPlayer];
+        let picEnemy = arrPicEnemy[navEnemy];
+        resizeImg(picPlayer, 8);
+        resizeImg(picEnemy, 5);
+        resizeImg(picLife, 5)
+        resizeImg(picCoin, 5)
+        boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer);
+        boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy);
+        boardPicCoin = boardPic(picCoin, positionCoin[0], positionCoin[1]);
+
+
+        function startPosition() {
+            xPlayer = window.innerWidth * 0.05;
+            yPlayer = window.innerHeight * 0.93 - picPlayer.height;
+
+            xEnemy = window.innerWidth * 0.85;
+            yEnemy = window.innerHeight * 0.93 - picEnemy.height;
+        }
+
+        function moveEnemy() {
+            function collisionEnemy() {
+                if (boardPicPlayer[0] > xEnemy && xPlayer < boardPicEnemy[0] && yPlayer < boardPicEnemy[1] && yEnemy < boardPicPlayer[1]) {
+                    countLife--;
+                    if (countLife <= 0) {
+                        let newGame = confirm('Gameover:(\n Хотите начать заново?');
+                        if (newGame) {
+                            countLife = 3;
+                        } else {
+                            clearTimeout(timerMoveEnemy);
+                            startPosition();
+                            return;
+                        }
                     }
+                    startPosition();
                 }
-                startPosition();
             }
+            collisionEnemy();
+            let timerMoveEnemy = setTimeout(() => {
+                if (xEnemy > (xPlayer + picPlayer.width)) {
+                    xEnemy--;
+                    navEnemy = 'left'
+                } else if ((xEnemy + picEnemy.width) < xPlayer) {
+                    xEnemy++;
+                    navEnemy = 'right'
+                }
+                draw();
+                moveEnemy();
+            }, speedEnemy);
         }
-        collisionEnemy ();
-        let timerMoveEnemy = setTimeout(()=>{
-            if(xEnemy>(xPlayer+picPlayer.width)){
-                xEnemy--;
-                navEnemy='left'
-            }else if((xEnemy+picEnemy.width)<xPlayer){
-                xEnemy++;
-                navEnemy='right'
-            }
-            draw();
-         moveEnemy();
-         },speedEnemy);
-        }
-        if(startGame){
+        if (startGame) {
             startPosition();
             newPositionCoin();
             moveEnemy();
-            startGame=false;
+            startGame = false;
         }
 
-    ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
-    ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width, picPlayer.height);
-    ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width, picEnemy.height);
-    ctx.drawImage(picCoin, positionCoin[0], positionCoin[1], picCoin.width,picCoin.height);
-    for(let i=0; i<countLife;i++){
-    let yLife = innerHeight*0.05;
-    let xLife = innerWidth*0.05+picLife.width*i;
-    ctx.drawImage(picLife, xLife, yLife, picLife.width, picLife.height);
-    }
-    function collisionCoin(){
-        if(checkCollision(xPlayer,positionCoin[0],yPlayer,positionCoin[1],boardPicPlayer[0],boardPicCoin[0],boardPicPlayer[1],boardPicCoin[1])){
-            countCoin++;
-            newPositionCoin();
+        ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
+        ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width, picPlayer.height);
+        ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width, picEnemy.height);
+        ctx.drawImage(picCoin, positionCoin[0], positionCoin[1], picCoin.width, picCoin.height);
+        for (let i = 0; i < countLife; i++) {
+            let yLife = innerHeight * 0.05;
+            let xLife = innerWidth * 0.05 + picLife.width * i;
+            ctx.drawImage(picLife, xLife, yLife, picLife.width, picLife.height);
         }
+
+        function collisionCoin() {
+            if (checkCollision(xPlayer, positionCoin[0], yPlayer, positionCoin[1], boardPicPlayer[0], boardPicCoin[0], boardPicPlayer[1], boardPicCoin[1])) {
+                countCoin++;
+                newPositionCoin();
+            }
+        }
+        collisionCoin();
+
+        printText("Count = " + countCoin, innerWidth * 0.05, innerHeight * 0.13, innerHeight * 0.15, "Aquamarine");
+    } else {
+        canvas.addEventListener('mousemove', (event) => {
+            if (ctx.isPointInPath(buttonStart, event.clientX, event.clientY)) {
+                drawButton('DarkBlue', 'Blue')
+            } else {
+                drawButton('Cyan', 'LightSeaGreen');
+            }
+        });
     }
-    collisionCoin();
-    function printText(text,x,y,size,color){
-        ctx.font = size+"Harlow Solid Italic";
-        ctx.fillStyle = color;
-        ctx.fillText(text,x,y);
-    }
-    printText("Count = "+countCoin,innerWidth*0.05,innerHeight*0.13,innerHeight*0.15,"Aquamarine");
 }
-
 picEnemyRight.onload = picPlayerLeft.onload = picPlayerLeft.onload = picBackground.onload = draw;
 
-document.addEventListener('keydown', (event)=>{
+document.addEventListener('keydown', (event) => {
     let KeyPressed = event.code;
     switch (KeyPressed) {
         case 'ArrowLeft':
@@ -176,9 +219,9 @@ document.addEventListener('keydown', (event)=>{
             navPlayer = 'right';
             soundStep.play();
             break;
-            case 'Enter':
-                startGame=true;
-                break
+        case 'Enter':
+            startGame = true;
+            break
     }
     draw();
 });
@@ -187,10 +230,10 @@ document.addEventListener('keyup', (event) => {
     switch (KeyPressed) {
         case 'ArrowLeft':
             soundStep.pause();
-        break;
+            break;
         case 'ArrowRight':
             soundStep.pause();
-        break;
+            break;
     }
     draw();
 });
